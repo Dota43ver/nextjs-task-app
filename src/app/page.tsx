ad1fabce6d8f5565e21789c7848ca4b52911e588
Dota43ver <facundoagueroc@gmail.com>
2023-09-12 11:51:15 -0300

    form e inputs

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,27 @@
-import React from 'react'
+import NoteForm from "@/components/NoteForm";
 
-function HomePage() {
-  return (
-    <div>HomePage</div>
-  )
+async function loadNotes() {
+  const res = await fetch("http://localhost:3000/api/notes");
+  const data = await res.json();
+  return data;
 }
 
-export default HomePage
+async function HomePage() {
+  const notes = await loadNotes();
+  return (
+    <div className="flex items-center justify-center h-screen">
+      <div>
+        <NoteForm />
+
+        {notes.map((note) => (
+          <div key={note.id} className="bg-slate-400 p-4">
+            <h1>{note.title}</h1>
+            <p>{note.content}</p>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default HomePage;
