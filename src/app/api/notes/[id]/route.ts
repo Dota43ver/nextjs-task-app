ad3260c57a496dadcd8fc375618b50d02a71aec0
Dota43ver <facundoagueroc@gmail.com>
2023-09-11 22:03:49 -0300

    rutas get put delete

---
+++ src/app/api/notes/[id]/route.ts
@@ -0,0 +1,71 @@
+import { NextResponse } from "next/server";
+import { Prisma } from "@prisma/client";
+import { prisma } from "@/libs/prisma";
+
+interface Params {
+  params: { id: string };
+}
+
+export async function GET(request: Request, { params }: Params) {
+  try {
+    const note = await prisma.note.findFirst({
+      where: {
+        id: Number(params.id),
+      },
+    });
+
+    if (!note) {
+      return NextResponse.json({ message: "Note not found" }, { status: 404 });
+    }
+    return NextResponse.json(note);
+  } catch (error) {
+    if (error instanceof Error)
+      return NextResponse.json(
+        {
+          message: error.message,
+        },
+        {
+          status: 500,
+        }
+      );
+  }
+}
+
+export async function DELETE(request: Request, { params }: Params) {
+  try {
+    const deletedNote = await prisma.note.delete({
+      where: {
+        id: Number(params.id),
+      },
+    });
+    if (!deletedNote) {
+      return NextResponse.json({ message: "Note not found" }, { status: 404 });
+    }
+    return NextResponse.json(deletedNote);
+  } catch (error) {
+    if (error instanceof Prisma.PrismaClientKnownRequestError) {
+      if (error.code === "P2025") {
+        return NextResponse.json(
+          {
+            message: "Note not found",
+          },
+          {
+            status: 404,
+          }
+        );
+      }
+      return NextResponse.json(
+        {
+          message: error.message,
+        },
+        { status: 500 }
+      );
+    }
+  }
+}
+
+export function PUT(request: Request) {
+  return NextResponse.json({
+    message: "updating single note...",
+  });
+}
